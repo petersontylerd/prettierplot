a2d74b9261ba8fec16985839f5b6b6736f17ec3f
Peterson <>
2019-06-21 16:35:10 -0500

    pypi test site submission

--- setup.py
+++ setup.py
@@ -1,27 +1,51 @@
-from distutils.core import setup
-setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
-)
+import setuptools
+
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+DESCRIPTION = 'Accelerate the end-to-end machine learning pipeline'
+DISTNAME = 'prettierplot'
+LICENSE = 'MIT'
+# DOWNLOAD_URL = 'https://pypi.org/project/'
+MAINTAINER = 'Tyler Peterson'
+MAINTAINER_EMAIL = 'petersontylerd@gmail.com'
+PROJECT_URLS = {
+    'Bug Tracker': 'https://github.com/Petersontylerd/prettierplot/issues'
+    ,'Source Code': 'https://github.com/Petersontylerd/prettierplot'
+}
+URL = 'https://github.com/Petersontylerd/prettierplot'
+VERSION = '0.0.10'
+
+def setup_package():
+    metadata = dict(name = DISTNAME
+                    ,maintainer = MAINTAINER
+                    ,maintainer_email = MAINTAINER_EMAIL
+                    ,description = DESCRIPTION
+                    ,keywords = ['Machine learning','Data science']
+                    ,license = LICENSE
+                    ,url = URL
+                    ,packages = setuptools.find_packages()
+                    ,project_urls = PROJECT_URLS
+                    ,version = VERSION
+                    ,long_description = LONG_DESCRIPTION
+                    ,classifiers = ['Development Status :: 2 - Pre-Alpha'
+                                    ,'Intended Audience :: Developers'
+                                    ,'Intended Audience :: Science/Research'
+                                    ,'Topic :: Scientific/Engineering'
+                                    ,'Topic :: Scientific/Engineering :: Information Analysis'
+                                    ,'Topic :: Scientific/Engineering :: Visualization'
+                                    ,'Topic :: Software Development'
+                                    ,'Topic :: Software Development :: Libraries :: Python Modules'
+                                    ,'License :: OSI Approved :: MIT License'
+                                    ,'Programming Language :: Python :: 3'
+                                    ,'Operating System :: OS Independent'
+                                 ]
+                    ,python_requires = ">=3.5"
+                    # ,install_requires = []
+                )
+
+    setuptools.setup(**metadata)
+
+if __name__ == "__main__":
+    setup_package()
