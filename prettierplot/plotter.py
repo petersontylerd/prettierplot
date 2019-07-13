aa3d7989aca26239a677b7d1134a118150d00f90
root <root@555c8f9c9076>
2019-07-13 19:02:15 +0000

    removes extraneous import statements

--- prettierplot/plotter.py
+++ prettierplot/plotter.py
@@ -2,17 +2,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.ticker as tkr
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-
-import sklearn.metrics as metrics
-import sklearn.preprocessing as prepocessing
-
-from statsmodels.stats.weightstats import ztest
-from statsmodels.stats.proportion import proportions_ztest
-from scipy import stats
 
 import prettierplot.style as style
 import prettierplot.util as util
@@ -22,12 +12,12 @@ class PrettierPlot():
     """
     Documentation:
         Description:
-            PrettierPlot is a class that contain methods for creating data visualization.
+            PrettierPlot creates high-quality data visualizations quickly and easily.
             Initialization of this class creates a plotting object of a chosen size and
-            orientation. Once the figure is initialized, the method makeCanvas is called
-            to create the figure axis or chosen number of axes. If multiple axes are 
-            specified, then multiple axes can be plotted on a single figure, or the 
-            position variable can be utilized to create a subplot arrangement.        
+            orientation. Once the figure is initialized, the method makeCanvas can be 
+            called to create the figure axis or chosen number of axes. Multiple axes can 
+            be plotted on a single figure, or the position variable can be utilized to 
+            create a subplot arrangement.        
     """        
     
     from .cat import prettyBarV, prettyBarH, prettyBoxPlotV, prettyBoxPlotH
@@ -45,11 +35,15 @@ class PrettierPlot():
         """
         Documentation:
             Description: 
-                Initialize PrettierPlot and determine chart proportions.
+                Initialize PrettierPlot and dynamically set chart size.
             Parameters:
                 chartProp : float or int, default = 15
                     Chart proportionality control. Determines relative size of figure size, axis labels, 
                     chart title, tick labels, tick marks.
+                plotOrientation : string, default = None
+                    Default value produces a plot that is wider than it is tall. Specifying 'tall' will 
+                    produce a taller, less wide plot. 'square' produces a square plot. 'wide' produces a
+                    plot that is much wide than it is tall.
         """
         self.chartProp = chartProp
         self.plotOrientation = plotOrientation
@@ -80,10 +74,8 @@ class PrettierPlot():
         Documentation:
             Description: 
                 Create Axes object. Add descriptive attributes such as titles and axis labels,
-                set font size and font color. Remove grid. Remove top and right spine. 
+                set font size and font color. Remove grid. Remove top and right spine.
             Parameters:
-                fig : figure object, default = plt.figure()
-                    matplotlib.pyplot figure object is a top level container for all plot elements.
                 title : string, default = '' (blank)
                     The title for the chart.
                 xLabel : string, default = '' (blank)
@@ -96,12 +88,8 @@ class PrettierPlot():
                 yShift : float, default = 0.8
                     Controls position of y-axis label. Higher values move label higher along axis. 
                     Intent is to align with top of axis.
-                position int (nrows, ncols, index) : default = 111
+                position : int (nrows, ncols, index), default = 111
                     Determine subplot position of plot.
-                orientation : string, default = None
-                    Default value produces a plot that is wider than it is tall. Specifying 'tall' will 
-                    produce a taller, less wide plot. 'square' produces a square plot. 'wide' produces a
-                    plot that is much wide than it is tall.
             Returns 
                 ax : Axes object
                     Contain figure elements
