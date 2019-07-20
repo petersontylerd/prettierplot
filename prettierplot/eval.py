fix correlation heatmap masking variable assignment

prettierplot/eval.py
@@ -42,7 +42,7 @@ def prettyProbPlot(self, x, plot):
     plot.get_lines()[1].set_color(style.styleGrey)
 
 
-def prettyCorrHeatmap(self, df, annot=False, cols=None, ax=None, vmin=-1.0, vmax=1.0):
+def prettyCorrHeatmap(self, df, annot=False, cols=None, mask = False, ax=None, vmin=-1.0, vmax=1.0):
     """
     Documentation:
         Description:
@@ -57,6 +57,9 @@ def prettyCorrHeatmap(self, df, annot=False, cols=None, ax=None, vmin=-1.0, vmax
                 value or not.
             cols : list, default = None
                 List of strings describing dataframe columns. Limits dataframe to select columns.
+            mask : boolean, default = False
+                Determines whether or not correlation table is masked such that only the lower
+                triangle appears.
             ax : Axes object, default = None
                 Axis on which to place visual.
             vmin : Float, default = -1.0
@@ -69,8 +72,8 @@ def prettyCorrHeatmap(self, df, annot=False, cols=None, ax=None, vmin=-1.0, vmax
     cols = corrMatrix.columns
 
     # generate a mask for the upper triangle
-    mask = np.zeros_like(corrMatrix, dtype=np.bool)
-    mask[np.triu_indices_from(mask)] = True
+    maskGrid = np.zeros_like(corrMatrix, dtype=np.bool)
+    maskGrid[np.triu_indices_from(maskGrid)] = True
 
     # adjust font size as needed
     if len(cols) <= 5:
@@ -89,7 +92,7 @@ def prettyCorrHeatmap(self, df, annot=False, cols=None, ax=None, vmin=-1.0, vmax
     # create heatmap using correlation matrix.
     g = sns.heatmap(
         corrMatrix,
-        mask=None,
+        mask=maskGrid if mask else None,
         vmin=vmin,
         vmax=vmax,
         annot=annot,
@@ -152,13 +155,6 @@ def prettyCorrHeatmapTarget(
     corrMatrix = df.corr()
     corrTop = corrMatrix[target.name]  # [:-1]
     corrTop = corrTop[abs(corrTop) > thresh].sort_values(ascending=False)
-
-    # # generate a mask for the upper triangle
-    # mask = np.zeros_like(corrMatrix, dtype=np.bool)
-    # mask[np.triu_indices_from(mask)] = True
-
-
-    display(pd.DataFrame(corrTop))
 
     if len(corrTop) <= 5:
         fontAdjust = 1.25

prettierplot/num.py
@@ -4,26 +4,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 
+from scipy.stats import linregress 
+
 import prettierplot.style as style
 import prettierplot.util as util
 
 
-def pretty2dScatter(
-    self,
-    x,
-    y,
-    df=None,
-    xUnits="f",
-    xTicks=None,
-    yUnits="f",
-    yTicks=None,
-    plotBuffer=True,
-    size=10,
-    axisLimits=True,
-    color=style.styleGrey,
-    facecolor="w",
-    ax=None,
-):
+def pretty2dScatter(self, x, y, df=None, xUnits="f", xTicks=None, yUnits="f", yTicks=None, plotBuffer=True,
+                    size=10, axisLimits=True, color=style.styleGrey, facecolor="w", ax=None,):
     """
     Documentation:
         Description: 
@@ -101,25 +89,8 @@ def pretty2dScatter(
     plt.tight_layout()
 
 
-def pretty2dScatterHue(
-    self,
-    x,
-    y,
-    target,
-    label,
-    df=None,
-    xUnits="f",
-    xTicks=None,
-    yUnits="f",
-    yTicks=None,
-    plotBuffer=True,
-    size=10,
-    axisLimits=True,
-    color=style.styleGrey,
-    facecolor="w",
-    bbox=(1.2, 0.9),
-    ax=None,
-):
+def pretty2dScatterHue(self, x, y, target, label, df=None, xUnits="f", xTicks=None, yUnits="f", yTicks=None,
+    plotBuffer=True, size=10, axisLimits=True, color=style.styleGrey, facecolor="w", bbox=(1.2, 0.9), ax=None,):
     """
     Documentation:
         Description: 
@@ -223,9 +194,7 @@ def pretty2dScatterHue(
     plt.tight_layout()
 
 
-def prettyDistPlot(
-    self, x, color, xUnits="f", yUnits="f", fit=None, xRotate=None, ax=None
-):
+def prettyDistPlot(self, x, color, xUnits="f", yUnits="f", fit=None, xRotate=None, ax=None):
     """
     Documentation:
         Description:
@@ -284,18 +253,8 @@ def prettyKdePlot(self, x, color, yUnits="f", xUnits="f", ax=None):
     util.utilLabelFormatter(ax=ax, xUnits=xUnits, yUnits=yUnits)
 
 
-def prettyRegPlot(
-    self,
-    x,
-    y,
-    data,
-    color=style.styleHexMid[0],
-    x_jitter=None,
-    xUnits="f",
-    yUnits="f",
-    xRotate=None,
-    ax=None,
-):
+def prettyRegPlot(self, x, y, data, color=style.styleHexMid[0], x_jitter=None, xUnits="f", yUnits="f", xRotate=None,
+                     ax=None,):
     """
     Documentation:
         Description: