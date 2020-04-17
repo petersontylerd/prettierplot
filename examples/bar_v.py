be23db9adf6868f5d7eb602ccfc08269637dc1d7
Karim Lahrichi <klahrichi@flinks.io>
2020-04-17 10:50:28 -0400

    Updated README with basic example

--- examples/bar_v.py
+++ examples/bar_v.py
@@ -1,19 +1,4 @@
-[![PyPI version](https://badge.fury.io/py/prettierplot.svg)](https://badge.fury.io/py/prettierplot)
-
-# prettierplot
-
-<i>"prettierplot is a Python library that makes it easy to create high-quality, polished data visualizations with minimal code."</i>
-
-## Installation
-
-```
-pip install prettierplot
-```
-
-## Example(s)
-
-```
-from prettierplot.plotter import PrettierPlot
+from m prettierplot.plotter import PrettierPlot
 from prettierplot import data
 import numpy as np
 
@@ -21,8 +6,7 @@ df = data.attrition()
 
 # capture unique EmployeeField values and frequency counts
 unique_vals, unique_counts = np.unique(
-    df[df["EducationField"].notnull()]["EducationField"], return_counts=True
-)
+df[df["EducationField"].notnull()]["EducationField"], return_counts=True)
 
 # create plotting instance
 p = PrettierPlot(chart_scale=10)
@@ -37,7 +21,4 @@ p.bar_v(
     label_rotate=45,
     x_tick_wrap=True
 )
-```
-
-![ex1](/examples/bar_v.png)
 
